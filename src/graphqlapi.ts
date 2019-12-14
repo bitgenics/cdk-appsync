6eb95a5ef57f15ea8cf04fd1715a43b803c1b866
Erwin van der Koogh <erwin@bitgenics.io>
2019-12-14 23:05:52 +1100

    Add logging support to CloudWatch

--- src/graphqlapi.ts
+++ src/graphqlapi.ts
@@ -1,10 +1,23 @@
 import { CfnGraphQLApi, CfnApiKey, CfnGraphQLSchema } from '@aws-cdk/aws-appsync'
 import { Construct, Resource } from '@aws-cdk/core'
+import { Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam'
+
+export const enum FieldLogLevel {
+  NONE = 'NONE',
+  ERROR = 'ERROR',
+  ALL = 'ALL',
+}
+
+export interface LogConfig {
+  excludeVerboseContent: boolean
+  fieldLogLevel: FieldLogLevel
+}
 
 export interface GraphQLApiProps {
   name: string
   authenticationType: string
   schema: string
+  logConfig?: LogConfig
 }
 
 export interface IGraphQLApi {
@@ -19,9 +32,22 @@ export class GraphQLApi extends Resource implements IGraphQLApi {
     const { name, authenticationType, schema } = props
     this.schema = schema
 
+    const logProps = props.logConfig || {
+      excludeVerboseContent: false,
+      fieldLogLevel: FieldLogLevel.ALL,
+    }
+    const role = new Role(this, 'Role', {
+      assumedBy: new ServicePrincipal('appsync.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSAppSyncPushToCloudWatchLogs'),
+      ],
+    })
+    const logConfig = { ...logProps, cloudWatchLogsRoleArn: role.roleArn }
+
     const cfn_graphqlApi = new CfnGraphQLApi(scope, `Resource`, {
       name,
       authenticationType,
+      logConfig,
     })
     this.apiId = cfn_graphqlApi.attrApiId
 
